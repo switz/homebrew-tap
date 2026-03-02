9ce4bf5a2f3161d836a50e1e2ef658725fff1542
Daniel Saewitz <git@saewitz.com>
2026-03-02 02:41:40 -0500

    Add plc-touch cask formula

---
+++ Casks/plc-touch.rb
@@ -0,0 +1,13 @@
+cask "plc-touch" do
+  version "0.1.0"
+  sha256 "136c433d8b39677776ff1846d27cabc2b580af29c4daa221ddbccd58cedf0692"
+
+  url "https://github.com/switz/plc-touch/releases/download/v#{version}/plc-touch.zip"
+  name "plc-touch"
+  desc "Secure Enclave based keys for ATProto PLC rotation keys"
+  homepage "https://github.com/switz/plc-touch"
+
+  app "plc-touch.app"
+
+  zap trash: "~/.config/plc-touch"
+end
